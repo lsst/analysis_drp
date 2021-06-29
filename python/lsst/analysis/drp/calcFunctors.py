f23e1f13e2022ad762aeb89c60d3a08433a09d9a
Sophie Reed <sophiereed@princeton.edu>
2021-06-28 21:18:31 -0500

    Make first plots for coadd QA

--- python/lsst/analysis/drp/calcFunctors.py
+++ python/lsst/analysis/drp/calcFunctors.py
@@ -1,6 +1,8 @@
-__all__ = ["SNCalculator", "KronFluxDivPsfFlux"]
+__all__ = ["SNCalculator", "KronFluxDivPsfFlux", "MagDiff"]
 
-from lsst.pipe.tasks.dataFrameActions import DivideColumns
+from lsst.pipe.tasks.dataFrameActions import DivideColumns, MultiColumnAction
+from lsst.pex.config import Field
+from astropy import units as u
 
 
 class SNCalculator(DivideColumns):
@@ -19,3 +21,44 @@ class KronFluxDivPsfFlux(DivideColumns):
         super().setDefaults()
         self.colA.column = "iKronFlux"
         self.colB.column = "iPsFlux"
+
+
+class MagDiff(MultiColumnAction):
+    """Calculate the difference between two magnitudes;
+    each magnitude is derived from a flux column.
+
+    Parameters
+    ----------
+    df : `pandas.core.frame.DataFrame`
+        The catalog to calculate the magnitude difference from.
+
+    Returns
+    -------
+    The magnitude difference in milli mags.
+
+    Notes
+    -----
+    The flux columns need to be in units (specifiable in
+    the fluxUnits1 and 2 config options) that can be converted
+    to janskies. This action doesn't have any calibration
+    information and assumes that the fluxes are already
+    calibrated.
+    """
+
+    col1 = Field(doc="Column to subtract from", dtype=str)
+    fluxUnits1 = Field(doc="Units for col1", dtype=str, default="nanojansky")
+    col2 = Field(doc="Column to subtract", dtype=str)
+    fluxUnits2 = Field(doc="Units for col2", dtype=str, default="nanojansky")
+
+    @property
+    def columns(self):
+        return (self.col1, self.col2)
+
+    def __call__(self, df):
+        flux1 = df[self.col1].values * u.Unit(self.fluxUnits1)
+        mag1 = flux1.to(u.ABmag)
+
+        flux2 = df[self.col2].values * u.Unit(self.fluxUnits2)
+        mag2 = flux2.to(u.ABmag)
+
+        return (mag1 - mag2)*1000.0
