lsst/analysis_drp

commit d8c758e5d6a7dc1785a2c7876dd837a286f4ccbc

Remove unused variables

python/lsst/analysis/drp/scatterPlot.py
@@ -501,13 +501,6 @@ class ScatterPlotWithTwoHistsTask(pipeBase.PipelineTask):
             plotMed = np.nanmedian(ysGalaxies)
         if yLims:
             ax.set_ylim(yLims[0], yLims[1])
-        else:
-            if len(ysStars) > 0:
-                [ys1, ys99] = np.nanpercentile(ysStars, [1, 99])  # noqa: F841
-            elif len(ysGalaxies) > 0:
-                [ys1, ys99] = np.nanpercentile(ysGalaxies, [1, 99])  # noqa: F841
-            else:
-                ys1, ys99 = np.nan, np.nan  # noqa: F841
 
             numSig = 4
             yLimMin = plotMed - numSig*sigMadYs

commit cd580081c0389cb6bbca6dd32ffd97530075b9d6

Add else block
Fix the bug introduced in d8c758e5d6a7dc1785a2c7876dd837a286f4ccbc

python/lsst/analysis/drp/scatterPlot.py
@@ -501,7 +501,7 @@ class ScatterPlotWithTwoHistsTask(pipeBase.PipelineTask):
             plotMed = np.nanmedian(ysGalaxies)
         if yLims:
             ax.set_ylim(yLims[0], yLims[1])
-
+        else:
             numSig = 4
             yLimMin = plotMed - numSig*sigMadYs
             yLimMax = plotMed + numSig*sigMadYs