526e0dd55d537ac50611661a8c7c7d55def89da4
Jim Bosch <jbosch@astro.princeton.edu>
2022-03-21 00:11:57 -0400

    WIP on plotResourceUsage dashboard.
    Refactored dependencies, added cost pie chart.
    
    Colors are wonky somehow.

--- dashboards/plotResourceUsage.py
+++ dashboards/plotResourceUsage.py
@@ -11,18 +11,11 @@ import bokeh
 
 
 from lsst.daf.butler import Butler
-BUTLER = Butler("/home/jbosch/LSST/tkt/DM-33963/EXPORTED_DATA", collections="u/jbosch/DM-33963")
 
 
 def naive_compute_cost(cpu_time, max_memory, memory_per_core=4E9):
     cores_occupied = np.ceil(max_memory / memory_per_core)
     return cpu_time * cores_occupied
-
-
-TABLES = {
-    ref.datasetType.name[:-len("_resource_usage")]: BUTLER.getDirect(ref)
-    for ref in BUTLER.registry.queryDatasets("*_resource_usage", components=False)
-}
 
 
 class ResourceDefinition(ABC):
@@ -79,66 +72,6 @@ class NaiveComputeCost(ResourceDefinition):
 RESOURCE_TYPES = [MaxMemory, Runtime, NaiveComputeCost]
 
 
-class TaskSelector(param.Parameterized):
-    task_summary = param.Parameter()
-    observation = param.Selector(
-        label="Observation Dimensions",
-        objects={"Unconstrained": None, "Yes": True, "No": False},
-        default=None,
-    )
-    tract = param.Selector(
-        label="Tract Dimensions",
-        objects={"Unconstrained": None, "Yes": True, "No": False},
-        default=None,
-    )
-    selector = param.ListSelector(objects=[], default=[])
-
-    @param.depends("observation", "tract", on_init=True, watch=True)
-    def _update_selector(self):
-        criteria = [np.ones(len(self.task_summary), dtype=bool)]
-        if self.observation:
-            criteria.append(self.task_summary["per_observation"])
-        elif self.observation is False:
-            criteria.append(np.logical_not(self.task_summary["per_observation"]))
-        if self.tract:
-            criteria.append(self.task_summary["per_tract"])
-        elif self.tract is False:
-            criteria.append(np.logical_not(self.task_summary["per_tract"]))
-        indexer = np.logical_and.reduce(criteria)
-        self.param.selector.objects = list(self.task_summary.index[indexer])
-
-    @param.depends("observation", "tract", on_init=True)
-    def info(self):
-        criteria = [np.ones(len(self.task_summary), dtype=bool)]
-        if self.observation:
-            criteria.append(self.task_summary["per_observation"])
-        elif self.observation is False:
-            criteria.append(np.logical_not(self.task_summary["per_observation"]))
-        if self.tract:
-            criteria.append(self.task_summary["per_tract"])
-        elif self.tract is False:
-            criteria.append(np.logical_not(self.task_summary["per_tract"]))
-        indexer = np.logical_and.reduce(criteria)
-        quanta = self.task_summary["quanta"]
-        cost = self.task_summary[NaiveComputeCost.__name__]
-        return panel.pane.Markdown(
-            f"{indexer.sum()}/{len(self.task_summary)} tasks, "
-            f"{quanta[indexer].sum()}/{quanta.sum()} quanta, "
-            f"{100*cost[indexer].sum()/cost.sum():0.02f}% of total compute cost",
-            sizing_mode="stretch_width",
-        )
-
-    def panel(self):
-        return panel.Column(
-            panel.Row(
-                self.param.observation,
-                self.param.tract,
-                self.info,
-            ),
-            panel.Param(self.param.selector, widgets={"selector": dict(sizing_mode="stretch_height")}),
-        )
-
-
 class ResourceUsageData:
 
     _OBSERVATION_DIMENSIONS = frozenset({"visit", "exposure"})
@@ -150,8 +83,6 @@ class ResourceUsageData:
             {
                 NaiveComputeCost.__name__: np.zeros(len(self.all_tasks), dtype=float),
                 "quanta": np.zeros(len(self.all_tasks), dtype=int),
-                "color": np.array(["#ffffff"] * len(self.all_tasks), dtype=(str, 8)),
-                "enabled": np.zeros(len(self.all_tasks), dtype=bool),
                 "per_observation": np.array(
                     [
                         not self._OBSERVATION_DIMENSIONS.isdisjoint(self._full_tables[task].columns)
@@ -170,7 +101,7 @@ class ResourceUsageData:
             index=self.all_tasks,
         )
         self._rebuild_summary()
-        self.task_selector = TaskSelector(task_summary=self.summary)
+        self._task_mask = np.ones(len(self.all_tasks), dtype=bool)
 
     @classmethod
     def _with_resource_columns(cls, table):
@@ -186,17 +117,6 @@ class ResourceUsageData:
         ]
         self.cost_sorter = np.argsort(self.summary[NaiveComputeCost.__name__])[::-1]
 
-    def _initialize_display_columns(self):
-        # Assign colors by rank in NaiveComputeCost total, and show the first
-        # 20 tasks in that ranking
-        for i, n, color in zip(
-            itertools.count(0),
-            self.cost_sorter,
-            itertools.cycle(bokeh.palettes.Category20[20])
-        ):
-            self.summary["color"][n] = color
-            self.summary["enabled"][n] = i < 20
-
     def get_filtered_table(self, task):
         return self._full_tables[task]
 
@@ -204,9 +124,115 @@ class ResourceUsageData:
         for task in self.all_tasks:
             yield task, self.get_filtered_table(task)
 
+    @property
+    def task_mask(self):
+        return self._task_mask
+
+    @task_mask.setter
+    def task_mask(self, mask):
+        self._task_mask[:] = mask
+
+    def plot_costs(self):
+        data = self.summary[self.task_mask].sort_values(NaiveComputeCost.__name__)
+        data["fraction"] = data[NaiveComputeCost.__name__] / data[NaiveComputeCost.__name__].sum()
+        data["angle"] = 2.0*np.pi*data["fraction"]
+        data["annotation"] = [
+            f"{task}: {cost:0.2f} core × hr ({fraction*100:0.2f}%)"
+            for task, cost, fraction in zip(data.index, data[NaiveComputeCost.__name__], data["fraction"])
+        ]
+        colors = np.empty(len(data), dtype=(str, 8))
+        for n, color in zip(
+            range(0, len(data)),
+            itertools.cycle(bokeh.palettes.Category20[20])
+        ):
+            colors[n] = color
+        data["color"] = colors
+
+        figure = bokeh.plotting.figure(
+            y_range=(-0.5, 0.5),
+            x_range=(-0.5, 0.5),
+            toolbar_location=None,
+            tools="hover",
+            tooltips="@annotation",
+        )
+        figure.wedge(
+            x=0,
+            y=0,
+            radius=0.4,
+            start_angle=bokeh.transform.cumsum('angle', include_zero=True),
+            end_angle=bokeh.transform.cumsum('angle'),
+            line_width=0.0,
+            fill_color="color",
+            source=data,
+        )
+        figure.axis.axis_label = None
+        figure.axis.visible = False
+        figure.grid.grid_line_color = None
+        return figure
+
+
+class Dashboard(param.Parameterized):
+    data = param.Parameter()
+    observation = param.Selector(
+        label="Include tasks with visit or exposure dimensions:",
+        objects={"Unconstrained": None, "Yes": True, "No": False},
+        default=None,
+    )
+    tract = param.Selector(
+        label="Include tasks with tract dimensions",
+        objects={"Unconstrained": None, "Yes": True, "No": False},
+        default=None,
+    )
+
+    def set_task_mask(self):
+        criteria = [np.ones(len(self.data.summary), dtype=bool)]
+        if self.observation:
+            criteria.append(self.data.summary["per_observation"])
+        elif self.observation is False:
+            criteria.append(np.logical_not(self.data.summary["per_observation"]))
+        if self.tract:
+            criteria.append(self.data.summary["per_tract"])
+        elif self.tract is False:
+            criteria.append(np.logical_not(self.data.summary["per_tract"]))
+        self.data.task_mask = np.logical_and.reduce(criteria)
+
+    @param.depends("observation", "tract", on_init=True)
+    def info(self):
+        self.set_task_mask()
+        quanta = self.data.summary["quanta"]
+        cost = self.data.summary[NaiveComputeCost.__name__]
+        return panel.pane.Markdown(
+            f"{self.data.task_mask.sum()}/{len(self.data.summary)} tasks, "
+            f"{quanta[self.data.task_mask].sum()}/{quanta.sum()} quanta, "
+            f"{100*cost[self.data.task_mask].sum()/cost.sum():0.02f}% of total compute cost",
+            sizing_mode="stretch_width",
+        )
+
+    @param.depends("observation", "tract", on_init=True)
+    def plot_costs(self):
+        self.set_task_mask()
+        return self.data.plot_costs()
+
     def panel(self):
-        return self.task_selector.panel()
+        return panel.Column(
+            panel.Row(
+                self.param.observation,
+                self.param.tract,
+                self.info,
+            ),
+            self.plot_costs,
+        )
 
 
-DATA = ResourceUsageData(TABLES)
-DATA.panel().servable()
+def main():
+    butler = Butler("/home/jbosch/LSST/tkt/DM-33963/EXPORTED_DATA", collections="u/jbosch/DM-33963")
+    tables = {
+        ref.datasetType.name[:-len("_resource_usage")]: butler.getDirect(ref)
+        for ref in butler.registry.queryDatasets("*_resource_usage", components=False)
+    }
+    data = ResourceUsageData(tables)
+    dashboard = Dashboard(data=data)
+    dashboard.panel().servable()
+
+
+main()
