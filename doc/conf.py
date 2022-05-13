8f160519364ebb63ad5a9c41637e6f6f4eb308a9
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2022-05-13 10:47:13 -0500

    Update conf.py in doc folder

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,13 @@
 """Sphinx configuration file for an LSST stack package.
-
 This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.analysis.drp
+from documenteer.conf.pipelinespkg import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='analysis_drp',
-    version=lsst.analysis.drp.version.__version__))
+project = "analysis_drp"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
